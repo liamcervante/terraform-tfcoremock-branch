5123851ce272afde74a166390f285b9827187d5c
Liam Cervante <liam.cervante@hashicorp.com>
2023-08-24 09:54:37 +0200

    v0.1.0

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    tfcoremock = {
+      source = "hashicorp/tfcoremock"
+    }
+  }
+
+#   testing {
+#     module_registry_source = "app.terraform.io/liamcervante/simple-copy/tfcoremock"
+#   }
+}
+
+variable "number" {
+    type = number
+}
+
+data "tfcoremock_simple_resource" "external" {
+  id = var.id
+}
+
+resource "tfcoremock_simple_resource" "internal" {
+  string = data.tfcoremock_simple_resource.external.string
+  number = data.tfcoremock_simple_resource.external.number
+  bool   = data.tfcoremock_simple_resource.external.bool
+}
