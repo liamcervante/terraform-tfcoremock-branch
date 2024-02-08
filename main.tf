e77be3fbbdc3a8b78b4639573faeeeaf810d50a7
Liam Cervante <liam.cervante@hashicorp.com>
2024-02-08 16:25:13 +0100

    remove comments

--- main.tf
+++ main.tf
@@ -6,9 +6,6 @@ terraform {
   }
 }
 
-# https://app.terraform.io/app/liamcervante/registry/modules/private/liamcervante/branch/tfcoremock/0.1.0
-# terraform test -cloud-run=app.terraform.io/liamcervante/branch/tfcoremock
-
 data "tfcoremock_simple_resource" "external" {
   id = var.id
 }
